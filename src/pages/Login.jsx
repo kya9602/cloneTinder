18638dcad543d6f9e6783c61f53fd42284ea4014
gyoung-ha <dlrudgk95@naver.com>
2022-09-15 15:36:16 +0900

    마이페이지

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import FormLogin from "../components/form/formLogin/FormLogin"
+import LoginHeader from "../components/header/LoginHeader";
+
+// 로그인 페이지
+
+const Login = () => {
+    return (
+        <div>
+            <LoginHeader/>
+            <FormLogin />
+        </div>
+    )
+}
+
+
+export default Login;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/header/Header";
+import MainList from "../components/main/MainList";
+
+
+const  Main=()=> {
+  return (
+    <>
+    <Header/>
+      <MainList></MainList>
+    </>
+  );
+}
+
+export default Main;
